3e7159f4b039ebcc804825681d975d86f38af856
Prayoch Rujira <prayoch@clockupstudio.com>
2019-02-08 15:11:55 +0700

    Extracted to states

--- Assets/Ninj/Scripts/com.clockupstudio/state/GameStateManager.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/state/GameStateManager.ts
@@ -3,52 +3,10 @@ namespace game {
     @ut.executeAfter(ut.Shared.UserCodeStart)
     export class GameStateManager extends ut.ComponentSystem {
 
-        private state: string = "INIT";
-        private restartKey = ut.Core2D.KeyCode.Return;
+        private state: GameState = new InitPlay();
 
         OnUpdate(): void {
-
-            if (this.state == "INIT") {
-                ut.EntityGroup.instantiate(this.world, 'game.GameScene');
-                ut.EntityGroup.instantiate(this.world, 'game.BossGroup');
-                ut.EntityGroup.instantiate(this.world, 'game.EnemyGroup');
-                this.state = "PLAY";
-            }
-
-            if (this.state == "PLAY") {
-
-                if (ut.Runtime.Input.getMouseButtonDown(0)) {
-                    ut.EntityGroup.destroyAll(this.world, 'game.GameScene');
-                    ut.EntityGroup.destroyAll(this.world, 'game.BossGroup');
-                    ut.EntityGroup.destroyAll(this.world, 'game.EnemyGroup');
-
-                    ut.EntityGroup.instantiate(this.world, 'game.GameOverScene');
-
-                    this.world.forEach([ut.Core2D.Camera2D], (camera) => {
-                        camera.backgroundColor = new ut.Core2D.Color(67 / 255, 82 / 255, 61 / 255, 1);
-                    });
-
-                    this.state = "GAMEOVER";
-                }
-            }
-
-            if (this.state == "GAMEOVER") {
-
-                if (ut.Runtime.Input.getKeyUp(this.restartKey)) {
-                    ut.EntityGroup.destroyAll(this.world, 'game.GameOverScene');
-
-                    ut.EntityGroup.instantiate(this.world, 'game.GameScene');
-                    ut.EntityGroup.instantiate(this.world, 'game.BossGroup');
-                    ut.EntityGroup.instantiate(this.world, 'game.EnemyGroup');
-
-                    this.world.forEach([ut.Core2D.Camera2D], (camera) => {
-                        camera.backgroundColor = new ut.Core2D.Color(199 / 255, 240 / 255, 216 / 255, 1);
-                    });
-
-                    this.state = "PLAY";
-                }
-            }
+            this.state = this.state.OnUpdate(this.world);
         }
-
     }
 }
---
+++ Assets/Ninj/Scripts/com.clockupstudio/state/States.ts
@@ -0,0 +1,64 @@
+namespace game {
+
+    export interface GameState {
+        OnUpdate(world: ut.World): GameState;
+    }
+    
+    export class InitPlay implements GameState {
+
+        OnUpdate(world: ut.World): GameState {
+            ut.EntityGroup.instantiate(world, 'game.GameScene');
+            ut.EntityGroup.instantiate(world, 'game.BossGroup');
+            ut.EntityGroup.instantiate(world, 'game.EnemyGroup');
+
+            world.forEach([ut.Core2D.Camera2D], (camera) => {
+                camera.backgroundColor = new ut.Core2D.Color(199 / 255, 240 / 255, 216 / 255, 1);
+            });
+
+            return new PlayState();
+        }
+    }
+
+    export class PlayState implements GameState {
+        OnUpdate(world: ut.World): GameState {
+            if (ut.Runtime.Input.getMouseButtonDown(0)) {
+                return new InitGameOver();
+            }
+            return this;
+        }
+    }
+
+    export class InitGameOver implements GameState {
+        OnUpdate(world: ut.World): GameState {
+            ut.EntityGroup.destroyAll(world, 'game.GameScene');
+            ut.EntityGroup.destroyAll(world, 'game.BossGroup');
+            ut.EntityGroup.destroyAll(world, 'game.EnemyGroup');
+
+            ut.EntityGroup.instantiate(world, 'game.GameOverScene');
+
+            world.forEach([ut.Core2D.Camera2D], (camera) => {
+                camera.backgroundColor = new ut.Core2D.Color(67 / 255, 82 / 255, 61 / 255, 1);
+            });
+
+            return new GameOverState();
+        }
+
+    }
+
+    export class GameOverState implements GameState {
+
+        private restartKey = ut.Core2D.KeyCode.Return;
+
+        OnUpdate(world: ut.World): GameState {
+            if (ut.Runtime.Input.getKeyUp(this.restartKey)) {
+                ut.EntityGroup.destroyAll(world, 'game.GameOverScene');
+                return new InitPlay();
+            }
+            
+            return this;
+        }
+
+    }
+
+
+}
