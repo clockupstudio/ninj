c582f4cf5089158e728f7b40bd57fb5505688b95
Prayoch Rujira <prayoch@clockupstudio.com>
2019-02-13 00:49:36 +0700

    Step backward whe player get hit

--- Assets/Ninj/Scripts/com.clockupstudio/player/HitSystem.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/player/HitSystem.ts
@@ -4,10 +4,11 @@ namespace com.clockupstudio.player {
 
         OnUpdate(): void {
             this.world.forEach(
-                [game.PlayerTag, ut.HitBox2D.HitBoxOverlapResults, game.UnitStatus],
-                (_, overlapResults, status) => {
+                [game.PlayerTag, ut.HitBox2D.HitBoxOverlapResults, game.UnitStatus, ut.Core2D.TransformLocalPosition],
+                (_, overlapResults, status, transform) => {
                     status.health -= 1;
                     status.damaged = true;
+                    transform.position = new Vector3(transform.position.x-1, transform.position.y, transform.position.z)
                 }
             )
         }
