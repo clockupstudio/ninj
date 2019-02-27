d2b3919a4f6d764156d9bf5d055f5e4e9e99738f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-02-27 17:30:01 +0700

    Implements move forward

---
+++ Assets/Scripts/ClockupStudio.Ninj.Player/Components/Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ClockupStudio.Ninj.Player.Components
+{
+    /// <summary>
+    /// Movement component control speed of game object.
+    /// </summary>
+    public class Movement : MonoBehaviour
+    {
+        public float Speed;
+    }
+
+}
---
+++ Assets/Scripts/ClockupStudio.Ninj.Player/Systems/MoveSystem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using ClockupStudio.Ninj.Player.Components;
+
+namespace ClockupStudio.Ninj.Player.Systems
+{
+    public class MoveSystem : ComponentSystem
+    {
+        protected override void OnUpdate()
+        {
+            var dt = Time.deltaTime;
+            ForEach((Movement movement, Transform transform, Rigidbody2D rb) =>
+            {
+                rb.MovePosition(new Vector2(transform.position.x + movement.Speed * dt, 0));
+            });
+        }
+    }
+
+}
