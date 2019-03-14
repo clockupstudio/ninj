ffe898f04da57c0c05fa69e8c25e3ebb0c89e62b
Prayoch Rujira <prayoch@clockupstudio.com>
2019-03-14 15:47:43 +0700

    Experiment limit jumping

--- Assets/Ninj/Scripts/com.clockupstudio/player/JumpingSystem.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/player/JumpingSystem.ts
@@ -3,13 +3,17 @@ namespace com.clockupstudio.player {
     export class JumpingSystem extends ut.ComponentSystem {
         OnUpdate(): void {
             this.world.forEach(
-                [game.PlayerTag, ut.Entity, game.Input, ut.Core2D.TransformLocalPosition],
-                (_, entity, input, transform) => {
+                [game.PlayerTag, game.Input, game.JumpingCount],
+                (_, input, jumpingCount) => {
+                    if( jumpingCount.count > 0) {
+                        return;
+                    }
+
                     if (ut.Runtime.Input.getKey(input.jump)) {
                         let player = this.world.getEntityByName("Player")
-                        let transform = this.world.getComponentData(player,ut.Core2D.TransformLocalPosition);
-                        
-                        ut.Tweens.TweenService.addTween(this.world, 
+                        let transform = this.world.getComponentData(player, ut.Core2D.TransformLocalPosition);
+
+                        ut.Tweens.TweenService.addTween(this.world,
                             player,
                             ut.Core2D.TransformLocalPosition.position.y,
                             transform.position.y, transform.position.y + 2,
@@ -18,7 +22,9 @@ namespace com.clockupstudio.player {
                             ut.Core2D.LoopMode.Once,
                             ut.Tweens.TweenFunc.Linear,
                             true
-                            );
+                        );
+
+                        jumpingCount.count += 1;
                     }
                 });
         }
