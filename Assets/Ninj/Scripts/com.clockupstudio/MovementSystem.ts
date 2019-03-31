3ed555137145056952d57b547c6cc11255657214
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-03-31 15:04:54 +0700

    Remove unused delta time

--- Assets/Ninj/Scripts/com.clockupstudio/MovementSystem.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/MovementSystem.ts
@@ -3,7 +3,6 @@ namespace com.clockupstudio {
     export class MovementSystem extends ut.ComponentSystem {
 
         OnUpdate(): void {
-            const dt = this.scheduler.deltaTime();
             this.world.forEach(
                 [game.InputDirection, game.Movement, ut.Entity, ut.Physics2D.Velocity2D],
                 (inputDirection, movement, entity, _) => {
