03c8257825e3e9a9ecf2fc3c218aa77962878d45
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-04-01 15:36:04 +0700

    Revert "Move HealthDecreaseSystem into separate file"
    This reverts commit b3fe73f5726a3c447e173641911e46a8c6f8a857.

--- Assets/Ninj/Scripts/com.clockupstudio/player/HitSystem.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/player/HitSystem.ts
@@ -36,4 +36,35 @@ namespace com.clockupstudio.player {
             )
         }
     }
+
+    /**
+     * HealthDecreaseSystem decrease health base from received damaged.
+     */
+    export class HealthDecreaseSystem extends ut.ComponentSystem {
+        OnUpdate(): void {
+            this.world.forEach(
+                [game.PlayerTag, game.Damaged, game.UnitStatus, ut.Core2D.TransformLocalPosition, ut.Core2D.TransformLocalScale, ut.Entity],
+                (_, damaged, status, transformPosition, transformScale, entity) => {
+                    // don't decrease if status still true.
+                    if (status.damaged) {
+                        return;
+                    }
+                    console.debug('decrease damaged');
+                    status.health -= damaged.damage;
+                    status.damaged = true;
+                    transformPosition.position = this.calculateReactionDirection(transformPosition.position, transformScale.scale)
+                    util.EntityUtil.removeComponent(this.world, entity, game.Damaged);
+                }
+            )
+        }
+
+        /**
+         * calculateReactionDirection calculate a new position base on current position and direction.
+         * @param position current position of entity.
+         * @param direction current direction of entity.
+         */
+        private calculateReactionDirection(position: Vector3, direction: Vector3): Vector3 {
+            return new Vector3(position.x + (-1 * direction.x), position.y, position.z);
+        }
+    }
 }
