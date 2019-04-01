bec7080eb04874833587c390028cba7a58ed2e24
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-04-01 15:35:57 +0700

    Revert "Remove unused system"
    This reverts commit 713ba80cd2549e9685d9f4600e4b694eaa085070.

---
+++ Assets/Ninj/Scripts/com.clockupstudio/player/IncreaseCollisionnAttackSystem.ts
@@ -0,0 +1,22 @@
+namespace com.clockupstudio.player {
+
+    export class IncreaseCollisionnAttackSystem extends ut.ComponentSystem {
+
+        OnUpdate(): void {
+            this.world.forEach(
+                [game.PlayerTag, game.InputAttack, ut.HitBox2D.RectHitBox2D],
+                (_, inputAttack, rectHitBox2D) => {
+                    const box = rectHitBox2D.box
+
+                    if (inputAttack.pressed) {
+                        box.width = 2.5
+                    } else {
+                        box.width = 1
+                    }
+
+                    rectHitBox2D.box = box
+                }
+            )
+        }
+    }
+}
