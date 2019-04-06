0d3ee78cd3dadd8f41f3e6f375780afb21d3b989
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2019-04-06 21:55:24 +0700

    Refactor spawn system
    This remove positions out of Spawner and extract set component data
    into separate functions.

--- Assets/Ninj/Scripts/com.clockupstudio/SpawnSystem.ts
+++ Assets/Ninj/Scripts/com.clockupstudio/SpawnSystem.ts
@@ -1,12 +1,56 @@
 namespace com.clockupstudio {
 
-    const bossGroup = 'game.BossGroup'
-
-    function randomPosition(): number {
-        return Math.random() > 0.5 ? 1 : 0;
+    enum Side {
+        Left,
+        Right
     }
 
-    /** New System */
+    function randomSide(): Side {
+        return Math.random() > 0.5 ? Side.Right : Side.Left;
+    }
+
+    function setPositionFromSide(world: ut.World, entity: ut.Entity, side: Side) {
+        let pos: Vector3;
+        if (side == Side.Left) {
+            pos = new Vector3(-10, 0);
+        } else {
+            pos = new Vector3(10, 0);
+        }
+        world.setComponentData(entity, new ut.Core2D.TransformLocalPosition(pos));
+    }
+
+    function setScaleFromSide(world: ut.World, entity: ut.Entity, side: Side) {
+        world.usingComponentData(
+            entity,
+            [ut.Core2D.TransformLocalScale],
+            (transformLocalScale) => {
+                const scale = transformLocalScale.scale;
+                if (side == Side.Left) {
+                    scale.x = Math.abs(scale.x)
+                } else {
+                    scale.x = scale.x > 0 ? -scale.x : scale.x
+                }
+                transformLocalScale.scale = scale;
+            })
+    }
+
+    function setDirectionFromSide(world: ut.World, entity: ut.Entity, side: Side) {
+        let dir: Vector2;
+        if (side == Side.Left) {
+            dir = new Vector2(1, 0);
+        } else {
+            dir = new Vector2(-1, 0);
+        }
+
+        util.EntityUtil.addComponent(world, entity, game.InputDirection);
+        const inputDir = new game.InputDirection();
+        inputDir.direction = dir;
+        world.setComponentData(entity, inputDir);
+    }
+
+    /**
+     * SpawnSystem spawn an enemy and random position that it spawn.
+     */
     export class SpawnSystem extends ut.ComponentSystem {
 
         OnUpdate(): void {
@@ -20,33 +64,15 @@ namespace com.clockupstudio {
 
                     spawner.time = 0
 
-                    let entity = ut.EntityGroup.instantiate(this.world, spawner.group);
-                    if (entity.length == 0) {
+                    let entities = ut.EntityGroup.instantiate(this.world, spawner.group);
+                    if (entities.length == 0) {
                         console.log('cannot find entity group');
                     }
 
-                    this.world.usingComponentData(
-                        entity[0],
-                        [ut.Core2D.TransformLocalPosition, ut.Core2D.TransformLocalScale, game.InputDirection],
-                        (transformLocalPosition, transformLocalScale, inputDirection) => {
-                            const spawnIndex = randomPosition()
-                            const pos = spawner.positions[spawnIndex]
-
-                            transformLocalPosition.position = pos
-                            console.log(`spawn at position: [${transformLocalPosition.position.x},${transformLocalPosition.position.y}]`)
-
-                            const scale = transformLocalScale.scale
-                            if (spawnIndex === 0) {
-                                scale.x = Math.abs(scale.x)
-                                inputDirection.direction = new Vector2(1, 0);
-                            } else {
-                                scale.x = scale.x > 0 ? -scale.x : scale.x
-                                inputDirection.direction = new Vector2(-1, 0);
-                            }
-
-                            transformLocalScale.scale = scale;
-                        }
-                    )
+                    const side = randomSide();
+                    setPositionFromSide(this.world, entities[0], side);
+                    setScaleFromSide(this.world, entities[0], side);
+                    setDirectionFromSide(this.world, entities[0], side);
                 }
             )
         }
